7a432ce8d704e2bc8c3cc725f0f22ce7dec17f49
Malugeli <alugeli@gmail.com>
2026-01-02 16:33:00 +0100

    ClientToScreen hat gefehlt. Projekt beendet

--- main.cpp
+++ main.cpp
@@ -8,25 +8,32 @@ int main()
 {
     SetProcessDpiAwarenessContext(DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2);
     HWND game = FindWindowW(NULL, L"masterduel");
+    if(!game){
+        std::cout << "Game not found";
+        return 1;
+    }
     RECT rect{};
     POINT p{};
     MSG msg;
     unique_hotkey a(NULL, 1, 0, VK_NUMPAD1);
-    // Versuche, die DWM-Rahmen zu bekommen (Visuell korrekt)
 
     while (GetMessage(&msg, NULL, 0, 0)) {
         if (msg.message == WM_HOTKEY) {
             std::system("cls");
-            HRESULT hr = DwmGetWindowAttribute(game, DWMWA_EXTENDED_FRAME_BOUNDS, &rect, sizeof(rect));
             GetCursorPos(&p);
             ScreenToClient(game, &p);
-            double length = rect.right - rect.left;
+            GetClientRect(game, &rect);
+            double length = rect.right - rect.left; //wir nutzen Client, Top und Left sind 0 aber zur Übersicht behalt ich das so
             double height = rect.bottom - rect.top;
-            std::cout << "Nimm x mal: " << p.x / length << '\n';
-            std::cout << "Nimm y mal: " << p.y / height << '\n' << '\n';
-            std::cout << "Pixel x: " << p.x << "||" << "Pixel y: " << p.y << '\n' << '\n';
-            std::cout << "Real x = " << rect.right * (p.x / length) << '\n';
-            std::cout << "rect.right = " << rect.right << " rect.left = " << rect.left << '\n' << "Length = " << rect.right - rect.left << '\n';
+            double faktorx = p.x / length; //wie viel ist der Pixel wo wir gerade sind vom ganzen Spiel?
+            double faktory = p.y / height;
+            std::cout << "X Faktor = " << faktorx << " Y Faktor = " << faktory << '\n';
+            std::cout << "X Koordinaten im Client: " << p.x << " Y Koordinaten im Client: " << p.y << '\n' << '\n';
+            p.x = rect.right * faktorx; //das was wir eigentlich machen wollten
+            p.y = rect.bottom * faktory;
+            ClientToScreen(game, &p); //das hat die ganze Zeit gefehlt..
+            std::cout << "X Koordinaten in Real: " << p.x << " Y Koordinaten in Real: " << p.y << '\n' << '\n';
+            SetCursorPos(p.x, p.y);          
         }
     }
 }
